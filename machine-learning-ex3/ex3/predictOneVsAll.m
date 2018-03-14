ea78131fdbc87dd96ea3cd5b605b4b6b88f4a32b
Script Kitty <hakuna.matata.kitty@gmail.com>
2018-03-13 23:06:36 -0500

    1 v all classifier prediction working

--- machine-learning-ex3/ex3/predictOneVsAll.m
+++ machine-learning-ex3/ex3/predictOneVsAll.m
@@ -28,10 +28,9 @@ X = [ones(m, 1) X];
 %       max element, for more information see 'help max'. If your examples 
 %       are in rows, then, you can use max(A, [], 2) to obtain the max 
 %       for each row.
-%       
-
-
-
+%     
+t = zeros(size(X, 1), 1);
+[t, p] = max(all_theta * X');
 
 
 
