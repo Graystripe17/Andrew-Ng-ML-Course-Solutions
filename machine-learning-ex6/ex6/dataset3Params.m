b7a9ce74153e997e5d0e7a1d06bf2fb27f36da15
Winston Van <winston.van7@gmail.com>
2018-06-17 13:28:26 -0500

    Parameters chosen

--- machine-learning-ex6/ex6/dataset3Params.m
+++ machine-learning-ex6/ex6/dataset3Params.m
@@ -18,10 +18,27 @@ sigma = 0.3;
 %               validation set. For example, 
 %                   predictions = svmPredict(model, Xval);
 %               will return the predictions on the cross validation set.
-%
-%  Note: You can compute the prediction error using 
+% %  Note: You can compute the prediction error using 
 %        mean(double(predictions ~= yval))
 %
+vals = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30];
+error = intmax('int32');
+for C_index = 1:numel(vals)
+    for sigma_index = 1:numel(vals)
+        model = svmTrain(X, y, vals(C_index), @(x1, x2) gaussianKernel(x1, x2, vals(sigma_index)), 1e-3, 20);
+        predictions = svmPredict(model, Xval);
+        new_error = mean(double(predictions ~= yval))
+        C
+        sigma
+        if (error > new_error)
+            C = vals(C_index);
+            sigma = vals(sigma_index);
+            error = new_error;
+            disp("HI");
+        end
+    end
+end
+
 
 
 
